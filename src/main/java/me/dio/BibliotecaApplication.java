41315ee8980c538b2ccf8a0373fb09ca0bc50378
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2024-07-27 00:30:32 -0300

    Bugfix CORS

--- src/main/java/me/dio/BibliotecaApplication.java
+++ src/main/java/me/dio/BibliotecaApplication.java
@@ -2,11 +2,12 @@ package me.dio;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Biblioteca API", version = "1.0", description = "API para gerenciamento de biblioteca"))
+@OpenAPIDefinition(servers = { @Server(url= "/", description = "Default Server URL")})
 public class BibliotecaApplication {
 
     public static void main(String[] args) {
